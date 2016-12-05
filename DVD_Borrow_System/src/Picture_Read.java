8fc4a67cc516560639f2fd93c1eaea99ecba9ffc
dccif <heyujieboy@163.com>
2016-12-05 23:12:48 +0800

    Add a Picture

--- DVD_Borrow_System/src/Picture_Read.java
+++ DVD_Borrow_System/src/Picture_Read.java
@@ -1,4 +1,40 @@
+import javax.swing.*;
 
-public class Picture_Read {
+public class Picture_Read{
 
+public static void main(String[] args) {
+		  Pic dvd = new Pic();
+		  dvd.addPic();
+	}
+	
+}
+class Pic extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	JButton[] buttons = new JButton[12]; 
+
+	ImageIcon ic = new ImageIcon("E:\\Source\\your-name.jpg");
+	
+	@SuppressWarnings("deprecation")
+	public void addPic(){
+		//super("DVD");
+
+		this.setTitle("DVD");	//设置窗体大小和标题
+		this.setSize(480,720);
+	
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//建立面板
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel();
+		
+		label.setIcon(ic);
+		panel.add(label);
+	
+		this.setContentPane(panel);
+		this.show();
+	    
+	   }  
 }
